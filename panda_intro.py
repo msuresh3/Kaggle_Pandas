6f1c0370c9038ea0e504c4d8c8a0ada93b74b225
Suresh Mariappan <nanotechsuresh@gmail.com>
2024-12-26 14:06:43 +0530

    Update panda_intro.py

--- panda_intro.py
+++ panda_intro.py
@@ -1,5 +1,5 @@
 import pandas as pd
 #df = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
-df = pd.DataFrame({'Apples':[30],'Bananas':[21]})
+#df = pd.DataFrame({'Apples':[30],'Bananas':[21]})
+df = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]},index = ['2017 Sales','2018 Sales'])
 print (df)
-
